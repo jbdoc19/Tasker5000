60271d4c93c7b78b0ede3992b7a6bd36f887cb63
jbdoc19 <132542985+jbdoc19@users.noreply.github.com>
2025-11-06 21:44:24 -0600

    Add dynamic neuroadaptive daily checklist

---
+++ scheduleBridge.js
@@ -0,0 +1,51 @@
+import { dayState } from "./dayState.js";
+import { WEEK_SCHEDULE } from "./scheduleModel.js";
+import {
+  CLINIC_BASKET_ITEMS,
+  CLINIC_DAYS,
+  CLINIC_OPTIONS,
+  SLOT_BLOCKS,
+  SLOT_TIMES,
+} from "./scheduleData.js";
+
+function cloneDayState() {
+  return {
+    currentDay: dayState.currentDay,
+    currentBlock: dayState.currentBlock,
+    clinicType: dayState.clinicType,
+    clinicSelections: { ...dayState.clinicSelections },
+    blockResidentPresence: { ...dayState.blockResidentPresence },
+    residentMap: { ...dayState.residentMap },
+  };
+}
+
+function cloneBasketItems() {
+  return CLINIC_BASKET_ITEMS.map(item => ({ ...item }));
+}
+
+function cloneSlotTimes() {
+  return Object.fromEntries(
+    Object.entries(SLOT_TIMES).map(([block, times]) => [block, [...times]]),
+  );
+}
+
+if (typeof window !== "undefined") {
+  window.TaskerSchedule = Object.assign(window.TaskerSchedule || {}, {
+    getWeekSchedule: () => WEEK_SCHEDULE,
+    getDayState: () => cloneDayState(),
+    getClinicBasketItems: () => cloneBasketItems(),
+    getClinicDays: () => [...CLINIC_DAYS],
+    getSlotBlocks: () => [...SLOT_BLOCKS],
+    getSlotTimes: () => cloneSlotTimes(),
+    getClinicOptions: () => [...CLINIC_OPTIONS],
+  });
+
+  window.dispatchEvent(
+    new CustomEvent("scheduleBridgeReady", {
+      detail: {
+        dayState: cloneDayState(),
+        clinicBasketItems: cloneBasketItems(),
+      },
+    }),
+  );
+}
