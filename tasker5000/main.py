2164bdfd85f303b364ba778d1810a3583c5df62c
jbdoc19 <132542985+jbdoc19@users.noreply.github.com>
2025-11-22 23:47:57 -0600

    Create main.py

---
+++ tasker5000/main.py
@@ -0,0 +1,17 @@
+{
+  "R_phys": 0.7,
+  "R_ment": 0.6,
+  "L": 200,
+  "dplan": 0.5,
+  "Csocial": 0.5,
+  "Gguilt": 0.3,
+  "Aanxiety": 0.2,
+  "N_vis": 3,
+  "Hhero": 0.2,
+  "Rnow": 0.6,
+  "Dtask": 0.3,
+  "Ssteps": 0.4,
+  "Uunfamiliar": 0.2,
+  "EHR_clunk": 0.3,
+  "availability_hours": 2
+}
